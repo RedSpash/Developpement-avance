011a62a1c68a76bfe6977ba6301495a8796c3e71
Red_Spash <red.spash.game@gmail.com>
2025-02-20 19:36:05 +0100

    feat(movie): add post method for /movie route

--- iut-project/lib/routes/movie.js
+++ iut-project/lib/routes/movie.js
@@ -13,5 +13,21 @@ module.exports = [
         handler: async (request, h) => {
             return await request.services().get();
         }
+    },
+    {
+        method: 'post', path: '/movie', options: {
+            auth: {
+                scope: ['admin']
+            }, tags: ['api'], validate: {
+                payload: Joi.object({
+                    title: Joi.string().min(3).example('Interstellar').required(),
+                    director: Joi.string().min(3).example('Christopher Nolan').required(),
+                    description: Joi.string().min(3).example('A movie about space').required(),
+                    releaseDate: Joi.date().example(new Date('2014-11-05')).required()
+                })
+            }
+        }, handler: async (request, h) => {
+            return request.services().create(request.payload);
+        }
     }
 ];
