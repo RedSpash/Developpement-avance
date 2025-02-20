c447c81eb4854725c6a22fbd4ce64bc11f568999
Red_Spash <red.spash.game@gmail.com>
2025-02-20 19:39:26 +0100

    feat(movie): add delete method for /movie/id route

--- iut-project/lib/routes/movie.js
+++ iut-project/lib/routes/movie.js
@@ -2,32 +2,41 @@
 
 const Joi = require('joi');
 
-module.exports = [
-    {
-        method: 'get', path: '/movies', options: {
-            auth: {
-                scope: ['user', 'admin']
-            }, tags: ['api']
-        },
-
-        handler: async (request, h) => {
-            return await request.services().get();
-        }
+module.exports = [{
+    method: 'get', path: '/movies', options: {
+        auth: {
+            scope: ['user', 'admin']
+        }, tags: ['api']
     },
-    {
-        method: 'post', path: '/movie', options: {
-            auth: {
-                scope: ['admin']
-            }, tags: ['api'], validate: {
-                payload: Joi.object({
-                    title: Joi.string().min(3).example('Interstellar').required(),
-                    director: Joi.string().min(3).example('Christopher Nolan').required(),
-                    description: Joi.string().min(3).example('A movie about space').required(),
-                    releaseDate: Joi.date().example(new Date('2014-11-05')).required()
-                })
-            }
-        }, handler: async (request, h) => {
-            return request.services().create(request.payload);
-        }
+
+    handler: async (request, h) => {
+        return await request.services().get();
     }
-];
+}, {
+    method: 'post', path: '/movie', options: {
+        auth: {
+            scope: ['admin']
+        }, tags: ['api'], validate: {
+            payload: Joi.object({
+                title: Joi.string().min(3).example('Interstellar').required(),
+                director: Joi.string().min(3).example('Christopher Nolan').required(),
+                description: Joi.string().min(3).example('A movie about space').required(),
+                releaseDate: Joi.date().example(new Date('2014-11-05')).required()
+            })
+        }
+    }, handler: async (request, h) => {
+        return request.services().create(request.payload);
+    }
+}, {
+    method: 'delete', path: '/movie/{id}', options: {
+        auth: {
+            scope: ['admin']
+        }, tags: ['api'], validate: {
+            params: Joi.object({
+                id: Joi.string().required().description('Identifier of the movie to delete')
+            })
+        }
+    }, handler: async (request, h) => {
+        return await request.services().delete(request.params.id);
+    }
+},];
