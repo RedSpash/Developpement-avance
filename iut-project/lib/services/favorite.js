4d1585c226b36533ba5b3fa34c19186a0b801bc3
Red_Spash <red.spash.game@gmail.com>
2025-02-23 11:02:56 +0100

    feat(favorite): add add method for favorite

--- iut-project/lib/services/favorite.js
+++ iut-project/lib/services/favorite.js
@@ -18,4 +18,26 @@ module.exports = class FavoriteService extends Service {
 
         return favorites;
     }
+
+    async add(userId, movieId) {
+        const { Favorite } = this.server.models();
+        const { Movie } = this.server.models();
+
+        const alreadyFavorite = await Favorite.query().select('*')
+            .where('id_user', '=', userId)
+            .where('id_movie', '=', movieId);
+
+        if (alreadyFavorite.length > 0) {
+            return Boom.conflict('Movie already in favorite');
+        }
+
+        const existingMovie = await Movie.query().select('*')
+            .where('id', '=', movieId);
+
+        if (existingMovie.length === 0) {
+            return Boom.notFound('Movie not found');
+        }
+
+        return Favorite.query().insert({ id_user: userId , id_movie: movieId });
+    }
 };
