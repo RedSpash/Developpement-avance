18ff51a2e91b41822cc860d7ae78edc4aae5553e
Red_Spash <red.spash.game@gmail.com>
2025-02-23 11:44:12 +0100

    feat(notification): add notification on movie edited

--- iut-project/lib/services/mail.js
+++ iut-project/lib/services/mail.js
@@ -10,11 +10,8 @@ module.exports = class MailService extends Service {
 
     createTransporter() {
         return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
+            host: process.env.EMAIL_HOST, port: process.env.EMAIL_PORT, auth: {
+                user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS
             }
         });
     }
@@ -28,14 +25,10 @@ module.exports = class MailService extends Service {
             from: process.env.EMAIL_USER,
             to: to.email,
             subject: 'Bienvenue',
-            text: `Bienvenue ${name},\n Merci de vous être inscrit sur notre site.`
+            text: 'Bienvenue ${name},\n Merci de vous être inscrit sur notre site.'
         };
 
-        try {
-            await transporter.sendMail(mailOptions);
-        } catch (error) {
-            throw new Error('Error sending email : ' + error);
-        }
+        await transporter.sendMail(mailOptions);
     };
 
     async sendNewMovieNotification(to, movie) {
@@ -44,7 +37,17 @@ module.exports = class MailService extends Service {
             to,
             subject: 'Nouveau film',
             text: 'Un nouveau film \'' + movie.title + '\' a été ajouté!',
-            html: `Venez découvrir le film : ${movie.title}`
+            html: 'Venez découvrir le film : ${movie.title}'
+        });
+    }
+
+    async sendPatchMovieNotification(to, movie) {
+        await this.createTransporter().sendMail({
+            from: process.env.EMAIL_USER,
+            to,
+            subject: 'Votre film favori a été modifié',
+            text: 'Le film \'' + movie.title + '\' a été modifié!',
+            html: 'Votre film favori \'' + movie.title + '\' a été modifié!'
         });
     }
 }
