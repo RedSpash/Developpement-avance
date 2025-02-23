6435be4027d208f8b6162a4bfe3b56af68b92a8d
Red_Spash <red.spash.game@gmail.com>
2025-02-23 11:06:27 +0100

    feat(favorite): add delete method for favorites

--- iut-project/lib/services/favorite.js
+++ iut-project/lib/services/favorite.js
@@ -22,10 +22,7 @@ module.exports = class FavoriteService extends Service {
     async add(userId, movieId) {
         const { Favorite } = this.server.models();
         const { Movie } = this.server.models();
-
-        const alreadyFavorite = await Favorite.query().select('*')
-            .where('id_user', '=', userId)
-            .where('id_movie', '=', movieId);
+        const alreadyFavorite = await this.getUserFavorites(userId, movieId);
 
         if (alreadyFavorite.length > 0) {
             return Boom.conflict('Movie already in favorite');
@@ -38,6 +35,33 @@ module.exports = class FavoriteService extends Service {
             return Boom.notFound('Movie not found');
         }
 
-        return Favorite.query().insert({ id_user: userId , id_movie: movieId });
+        return Favorite.query().insert({ user_id: userId , movie_id: movieId });
+    }
+
+    async getUserFavorites(userId, movieId) {
+        const { Favorite } = this.server.models();
+
+        return Favorite.query().select('*')
+            .where('user_id', '=', userId)
+            .where('movie_id', '=', movieId);
+    }
+
+    async delete(userId, movieId) {
+        const { Favorite } = this.server.models();
+        const { Movie } = this.server.models();
+
+        if (await this.getUserFavorites(userId, movieId).length === 0) {
+            return Boom.conflict('Movie not in favorite');
+        }
+
+        const existingMovie = await Movie.query().select('*')
+            .where('id', '=', movieId);
+        if (existingMovie.length === 0) {
+            return Boom.notFound('Movie not found');
+        }
+
+        return Favorite.query().delete()
+            .where('user_id', '=', userId)
+            .where('movie_id', '=', movieId);
     }
 };
