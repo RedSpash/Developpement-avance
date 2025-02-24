17a842ce081665a23c9024f7bebce028ecb6c097
Red_Spash <red.spash.game@gmail.com>
2025-02-24 18:12:09 +0100

    feat(exportation): add exportation service

---
+++ iut-project/lib/services/exportation.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const {Service} = require('@hapipal/schmervice');
+const amqp = require('amqplib');
+const Dotenv = require('dotenv');
+
+Dotenv.config({path: `.env`});
+
+module.exports = class ExportationService extends Service {
+
+    async get(request, to) {
+        const {movieService} = request.services();
+        const movies = await movieService.get();
+
+        const connection = await amqp.connect(process.env.RABBITMQ_URL);
+        const channel = await connection.createChannel();
+        await channel.assertQueue(process.env.QUEUE_NAME, {durable: true});
+
+        const message = JSON.stringify({to, movies});
+        channel.sendToQueue(process.env.QUEUE_NAME, Buffer.from(message), {persistent: true});
+
+        return {message: 'You will receive an email shortly with the exported data'};
+    }
+};
